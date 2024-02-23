983392e45690e5952eacf23b88de4a3e93aa2a42
BennyShinichi <145767884+BennyShinichi@users.noreply.github.com>
2024-02-23 14:42:32 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Starting the game...");
+
+        bool gameLoop = true;
+        while(gameLoop)
+        {
+            Console.WriteLine("\nPress \'Q\' when you want to quit");
+            Console.WriteLine("\nOr press any other key to start the game");
+
+            if(Console.ReadKey(true).Key == ConsoleKey.Q)
+            {
+                gameLoop = false;
+                Console.WriteLine("Quitting the game...");
+            }
+            else
+            {
+                // Explore the word in this code block
+                // Console.WriteLine("")
+            }
+        }
+        // For messages or functions after quitting the game
+    }
+}
