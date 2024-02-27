6c3869fcc9a9513a44e7a5bdf71175eb41458950
Max Bretherton <maxobminecraft@gmail.com>
2024-02-27 14:36:27 +0100

    Added the layworks for healing items

---
+++ HealingItem.cs
@@ -0,0 +1,10 @@
+public class HealingItem{
+    public string Name;
+    public string Description;
+    public int HealAmount;
+    public HealingItem(string name, string description, int healAmount){
+        Name = name;
+        Description = description;
+        HealAmount = healAmount;
+    }
+}
