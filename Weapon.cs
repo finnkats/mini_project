ffd8a2b5d7220d3224cee8a5a6195dd43b40afa0
Finn Kats <finnkats@pm.me>
2024-02-27 13:36:02 +0100

    added Weapon.cs

---
+++ Weapon.cs
@@ -0,0 +1,12 @@
+public class Weapon{
+    public int ID;
+    public double MaximumDamage;
+    public string Name;
+
+    public Weapon(int id, string name, double maxDamage){
+        ID = id;
+        Name = name;
+        MaximumDamage = maxDamage;
+    }
+
+}
